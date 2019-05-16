121f651e0758bf2f63c46eba62cec1c156896a02
JakubMu <jakub.murin@erstegroup.com>
2019-05-16 10:07:23 +0200

    add constants file

---
+++ app/constants.ts
@@ -0,0 +1,15 @@
+export const MAX_QUALITY = 50;
+export const ZERO_QUALITY = 0;
+export const LAST_DATE = 0;
+
+export const INCREASE = {
+  BY_ONE: 1,
+  BY_TWO: 2,
+  BY_THREE: 3
+};
+
+export const DECREASE = {
+  BY_ONE: 1,
+  BY_TWO: 2
+};
+
